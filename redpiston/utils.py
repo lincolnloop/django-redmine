aee7a2680ee5ee2c337a99d16177abc2160ae14d
Peter Baumgartner <pete@lincolnloop.com>
2010-03-09 23:20:42 -0700

    Renamed application and more tweaking to models

--- redpiston/utils.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import get_model
-
-def load_generic_object(model_name, pk):
-    model = get_model('redpiston', model_name)
-    try:
-        return model.objects.get(pk=pk)
-    except model.DoesNotExist:
-        return None
