feef0233d3a6021b19023ddbe3b7258c2ca319ff
Peter Baumgartner <pete@lincolnloop.com>
2010-03-23 10:55:01 -0600

    Made easy_install-able

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-redmine',
+    version='0.0.1',
+    description='Django connector to a Redmine database',
+    author='Peter Baumgartner',
+    author_email='pete@lincolnloop.com',
+    url='http://github.com/lincolnloop/django-redmine',
+    packages=find_packages(),
+)
